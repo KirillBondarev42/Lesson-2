dfca2c9d8b97208ccaca502782d371f18953e23d
KirillBondarev42 <72997639+KirillBondarev42@users.noreply.github.com>
2020-11-04 18:23:22 +0300

    Added sum

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
-void main ()
+int sum (int a, int b) // функция вычисления суммы двух чисел
 {
-	printf("%cHello,world!\r\n");
-	return;
+    int c = a + b;  // вычисляем сумму
+	return c;  // возвращаем сумму
+}
+
+
+int main ()
+{
+	printf("%sHello, World!\n", );
+	int a,b,c;
+	printf("a= ");
+	scanf("%d", &a); // ввод числа a
+	printf("b= ");
+	scanf("%d", &b); // ввод числа b
+	c = sum(a, b); // вызов функции суммы
+	printf("The sum of two numbers is %d\n", c);
+	return 0;
 }
